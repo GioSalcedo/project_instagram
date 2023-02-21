593e4e42e015b2b973fd46da45ec0986b2165e17
Giovanny Salcedo <gsalcedo26@gmail.com>
2023-02-21 13:40:57 -0500

    views done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,48 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @comment = Comment.where(post_id: params[:post_id])
   end
 
-  def show
-  end
+  def show;end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+    if @comment.save
+      redirect_to post_comment_path(@comment.post_id, @comment.id)
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @comment.update(comment_params)
+      redirect_to post_comment_path(@comment.post_id, @comment.id)
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  def destoy
+    @comment.destroy
+    redirect_to post_comment_path(@comment.post_id)
   end
 
-  def set_comment
+  private 
+
+  def set_post
+    @comment = Comment.find(params[:id])
   end
 
-  def comment_params
+  def post_params
+    params.require(:comment).permit(:content)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,45 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @post = Post.where(user_id: params[:user_id])
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user_id = params[:user_id]
+    if @post.save
+      redirect_to user_post_path(@post.user_id, @post.id)
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
+  def edit;end
 
   def update
+    if @post.update(post_params)
+      redirect_to user_post_path(@post.user_id, @post.id)
+    else
+      render :edit
+    end
   end
 
-  def destroy
+  def destoy
+    @post.destroy
+    redirect_to user_post_path(@post.user_id)
   end
 
   def set_post
+    @post = Post.find(params[:id])
   end
 
   def post_params
+    params.require(:post).permit(:description, :images)
   end
 end
